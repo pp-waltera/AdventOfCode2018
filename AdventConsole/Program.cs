52c1c2115b5ea187e8ffc0deeff1ffa9ea9bfb1b
Alexander Walter <ingabrevtack@gmail.com>
2018-12-01 15:04:19 +0100

    Day 1. Happy coding!

---
+++ AdventConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿using Solutions;
+using System;
+
+namespace AdventConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Day1 day1 = new Day1();
+            Console.WriteLine($"Day 1, part 1:\t{day1.Part1()}");
+            Console.WriteLine($"Day 1, part 2:\t{day1.Part2()}");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Solutions/Day1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Solutions
+{
+    public class Day1
+    {
+        internal readonly IEnumerable<int> _lines = File.ReadLines("C:/Users/Alexander/Desktop/input.txt").Select(x => Convert.ToInt32(x)).ToList();
+
+        public int Part1()
+        {
+            var sum = 0;
+
+            foreach (var line in _lines)
+            {
+                sum += line;
+            }
+
+            return sum;
+        }
+
+        public int Part2()
+        {
+            var sum = 0;
+            bool sumIsUnique = true;
+            HashSet<int> sumSet = new HashSet<int>();
+
+            while (sumIsUnique)
+            {
+                foreach (var line in _lines)
+                {
+                    sum += line;
+
+                    if (!sumSet.Contains(sum)) sumSet.Add(sum);
+                    else
+                    {
+                        sumIsUnique = false;
+                        break;
+                    }
+                }
+            }
+
+            return sum;
+        }
+    }
+}
